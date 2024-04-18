871f14f6d6e8926bfd77f1739c0912b6201b1266
Scott Sickles <scottsickles@gmail.com>
2024-04-18 13:33:07 -0400

    add scheduler permissions

--- access.tf
+++ access.tf
@@ -1,27 +1,9 @@
 resource "aws_iam_role_policy_attachment" "batch-job-creation" {
   role       = local.role_name
-  policy_arn = aws_iam_policy.batch-job-creation.arn
+  policy_arn = "arn:aws:iam::aws:policy/AWSBatchFullAccess"
 }
 
-resource "aws_iam_policy" "batch-job-creation" {
-  name        = local.resource_name
-  policy      = data.aws_iam_policy_document.batch-job-creation.json
-  description = "Policy to enable the management and creation of batch jobs"
-}
-
-data "aws_iam_policy_document" "batch-job-creation" {
-  statement {
-    effect = "Allow"
-    actions = [
-      "batch:SubmitJob",
-      "batch:DescribeJobs",
-      "batch:TerminateJob",
-      "batch:ListJobs",
-      "batch:ListJobDefinitions",
-      "batch:DescribeJobDefinitions",
-      "batch:DescribeJobQueues",
-      "batch:ListJobQueues",
-    ]
-    resources = ["*"]
-  }
+resource "aws_iam_role_policy_attachment" "scheduler-admin" {
+  role       = local.role_name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEventBridgeSchedulerFullAccess"
 }
